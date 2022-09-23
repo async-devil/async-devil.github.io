7d78c5e0473477b04aca9f3f12c2ea2667b648c1
async-devil <async.devil@gmail.com>
2022-09-24 00:19:46 +0300

    feat: update index html and ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-import "./styles.scss";
-import "./normalize.scss";
+import "./scss/styles.scss";
+import "./scss/normalize.scss";
+import "swiper/scss";
 
-import { test } from "./ts/lib";
+import Swiper, { Navigation } from "swiper";
 
-test();
+import { callBackground } from "./ts/background/background";
+
+new Swiper(".swiper", {
+	modules: [Navigation],
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	loop: true,
+});
+
+callBackground();
