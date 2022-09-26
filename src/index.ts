db8380923dc45928004c75949ed433f789255721
async-devil <async.devil@gmail.com>
2022-09-26 23:30:39 +0300

    feat: remove swiper

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
 import "./scss/styles.scss";
 import "./scss/normalize.scss";
-import "swiper/scss";
-
-import Swiper, { Navigation } from "swiper";
 
 import { callBackground } from "./ts/background/background";
-
-new Swiper(".swiper", {
-	modules: [Navigation],
-
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-
-	loop: true,
-});
 
 callBackground();
